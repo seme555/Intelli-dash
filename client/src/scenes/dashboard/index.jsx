c32e99ee59942faef937d55189453f03bcf2d794
Semere <semeabe@gmail.com>
2024-02-09 13:00:00 +0000

    Progress on client\src\scenes\dashboard\index.jsx (+11 lines)

---
+++ client/src/scenes/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+  Close,
+} from "@mui/icons-material";
+import {
+  Box,
