78db46db12f6c57f357dd81c941df8420f43102f
Semere <semeabe@gmail.com>
2024-02-09 10:00:00 +0000

    Progress on client\src\scenes\overview\index.jsx (+6 lines)

---
+++ client/src/scenes/overview/index.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { FormControl, MenuItem, InputLabel, Box, Select } from "@mui/material";
+
+import { Header, OverviewChart } from "components";
+
+// Overview
