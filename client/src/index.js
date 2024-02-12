87c63d928a71574b63842fb312b446a8f9e0744e
Semere <semeabe@gmail.com>
2024-02-12 07:00:00 +0000

    Progress on client\src\index.js (+6 lines)

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
