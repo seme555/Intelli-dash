a8f7c10601ccbe43f5b946af70fc6a6a77c3fea2
Semere <semeabe@gmail.com>
2024-02-14 14:00:00 +0000

    Progress on client\src\components\index.js (+12 lines)

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+// Contains components to be exported in other files
+// ! Don't remove anything from here
+
+import BreakdownChart from "./BreakdownChart";
+import CustomColumnMenu from "./DataGridCustomColumnMenu";
+import DataGridCustomToolbar from "./DataGridCustomToolbar";
+import FlexBetween from "./FlexBetween";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import OverviewChart from "./OverviewChart";
+import Sidebar from "./Sidebar";
+import StatBox from "./StatBox";
