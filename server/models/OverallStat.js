30b52ddc1dc004e7a9d6a652dafff867f6d49648
Semere <semeabe@gmail.com>
2024-02-14 15:00:00 +0000

    Progress on server\models\OverallStat.js (+10 lines)

---
+++ server/models/OverallStat.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+// Overall Stat Schema
+const OverallStatSchema = new mongoose.Schema(
+  {
+    totalCustomers: Number,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
