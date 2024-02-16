4ba156da434f63a5a8b94e20360b69300a2629f1
Semere <semeabe@gmail.com>
2024-02-16 07:00:00 +0000

    Progress on server\data\index.js (+10 lines)

---
+++ server/data/index.js
@@ -0,0 +1,10 @@
+// Contains Data to be inserted into database, you can add new contents here
+// Don't remove this file
+
+// Affiliate Stat data
+export const dataAffiliateStat = [
+  {
+    _id: "6371251df03239e680000033",
+    userId: "63701cc1f03239f09e00018a",
+    affiliateSales: [
+      "63701d74f0323986f300013e",
