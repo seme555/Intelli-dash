9355c4675f102cf696d6007b47e1079de008391b
Semere <semeabe@gmail.com>
2024-02-16 18:00:00 +0000

    Progress on client\src\components\Navbar.jsx (+12 lines)

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import {
+  AppBar,
+  useTheme,
+  Toolbar,
+  Menu,
+  MenuItem,
+  Button,
+  Box,
+  Typography,
