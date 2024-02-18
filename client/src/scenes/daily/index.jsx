7e4195b0ab48bd986f0b28d7b9e481b06a6d6de0
Semere <semeabe@gmail.com>
2024-02-18 08:00:00 +0000

    Progress on client\src\scenes\daily\index.jsx (+7 lines)

---
+++ client/src/scenes/daily/index.jsx
@@ -0,0 +1,7 @@
+import React, { useMemo, useState } from "react";
+import { Box, CircularProgress, useTheme } from "@mui/material";
+import DatePicker from "react-datepicker";
+import { ResponsiveLine } from "@nivo/line";
+
+import { useGetSalesQuery } from "state/api";
+import { Header } from "components";
