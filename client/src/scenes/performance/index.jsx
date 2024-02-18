b7e4a0a91128cec36574f243c84181918d33bed3
Semere <semeabe@gmail.com>
2024-02-18 13:00:00 +0000

    Progress on client\src\scenes\performance\index.jsx (+9 lines)

---
+++ client/src/scenes/performance/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
+
+import { useGetUserPerformanceQuery } from "state/api";
+import { Header, CustomColumnMenu } from "components";
+
+// Performance
