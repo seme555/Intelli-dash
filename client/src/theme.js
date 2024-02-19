ded0ff2c4058e35a589ee3d5cdaadf543ab1ff08
Semere <semeabe@gmail.com>
2024-02-19 10:00:00 +0000

    Progress on client\src\theme.js (+8 lines)

---
+++ client/src/theme.js
@@ -0,0 +1,8 @@
+// color design tokens export
+export const tokensDark = {
+  grey: {
+    0: "#ffffff", // manually adjusted
+    10: "#f6f6f6", // manually adjusted
+    50: "#f0f0f0", // manually adjusted
+    100: "#e0e0e0",
+    200: "#c2c2c2",
