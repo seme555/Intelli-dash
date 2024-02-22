4d74bf67619a8fa7e10614d97dde8cefa4d1e9a9
Semere <semeabe@gmail.com>
2024-02-22 21:00:00 +0000

    Progress on client\src\components\StatBox.jsx (+5 lines)

--- client/src/components/StatBox.jsx
+++ client/src/components/StatBox.jsx
@@ -4,3 +4,8 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { FlexBetween } from ".";
 
 // Stat Box
+const StatBox = ({ title, value, increase, icon, description }) => {
+  // theme
+  const theme = useTheme();
+  return (
+    <Box
