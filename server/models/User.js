b40e3cf8c0e9c734cf4fadf2810121820345b159
Semere <semeabe@gmail.com>
2024-02-22 23:00:00 +0000

    Progress on server\models\User.js (+11 lines)

--- server/models/User.js
+++ server/models/User.js
@@ -8,3 +8,14 @@ const UserSchema = new mongoose.Schema(
       required: true,
       min: 2,
       max: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
