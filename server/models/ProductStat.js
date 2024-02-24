930a38f5518e07b6336d06a391763f76d96d16a7
Semere <semeabe@gmail.com>
2024-02-24 12:00:00 +0000

    Progress on server\models\ProductStat.js (+7 lines)

--- server/models/ProductStat.js
+++ server/models/ProductStat.js
@@ -1,3 +1,10 @@
 import mongoose from "mongoose";
 
 // Product Stat Schema
+const ProductStatSchema = new mongoose.Schema(
+  {
+    productId: String,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
