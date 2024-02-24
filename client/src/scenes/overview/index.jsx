105d19a4a68ada1c468e21d47b62766dfa04e440
Semere <semeabe@gmail.com>
2024-02-24 19:00:00 +0000

    Progress on client\src\scenes\overview\index.jsx (+10 lines)

--- client/src/scenes/overview/index.jsx
+++ client/src/scenes/overview/index.jsx
@@ -4,3 +4,13 @@ import { FormControl, MenuItem, InputLabel, Box, Select } from "@mui/material";
 import { Header, OverviewChart } from "components";
 
 // Overview
+const Overview = () => {
+  // view - units/sales
+  const [view, setView] = useState("units");
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      {/* Header */}
+      <Header
+        title="OVERVIEW"
+        subtitle="Overview of general revenue and profit"
