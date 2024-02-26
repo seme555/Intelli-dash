7d192907f90ac4a583ec5eabc3339a878cad0da6
Semere <semeabe@gmail.com>
2024-02-26 15:00:00 +0000

    Progress on client\src\components\DataGridCustomColumnMenu.jsx (+11 lines)

--- client/src/components/DataGridCustomColumnMenu.jsx
+++ client/src/components/DataGridCustomColumnMenu.jsx
@@ -4,3 +4,14 @@ import {
   GridFilterMenuItem,
   HideGridColMenuItem,
 } from "@mui/x-data-grid";
+
+// Custom column menu
+const CustomColumnMenu = (props) => {
+  const { hideMenu, currentColumn, open } = props;
+  return (
+    <GridColumnMenuContainer
+      hideMenu={hideMenu}
+      currentColumn={currentColumn}
+      open={open}
+    >
+      {/* Menu Items */}
