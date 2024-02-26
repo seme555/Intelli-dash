419f944d16998bc4ee763e183ce4ea3502f4e3be
Semere <semeabe@gmail.com>
2024-02-26 16:00:00 +0000

    Progress on client\src\scenes\monthly\index.jsx (+11 lines)

--- client/src/scenes/monthly/index.jsx
+++ client/src/scenes/monthly/index.jsx
@@ -4,3 +4,14 @@ import { ResponsiveLine } from "@nivo/line";
 
 import { useGetSalesQuery } from "state/api";
 import { Header } from "components";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+// Monthly
+const Monthly = () => {
+  // get data
+  const { data } = useGetSalesQuery();
+  // theme
+  const theme = useTheme();
+
+  // formatted data
