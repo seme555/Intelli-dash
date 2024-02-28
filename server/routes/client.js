89c49f9c93f5d7688c3d600eaa0e90153a721276
Semere <semeabe@gmail.com>
2024-02-28 18:00:00 +0000

    Progress on server\routes\client.js (+6 lines)

--- server/routes/client.js
+++ server/routes/client.js
@@ -8,3 +8,9 @@ import {
 } from "../controllers/client.js";
 
 const router = express.Router();
+
+// Routes
+router.get("/products", getProducts);
+router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
