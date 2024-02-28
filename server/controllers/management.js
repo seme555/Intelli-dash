7d3dd797366af09619dd382fb5a78046253b1876
Semere <semeabe@gmail.com>
2024-02-28 11:00:00 +0000

    Progress on server\controllers\management.js (+11 lines)

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -4,3 +4,14 @@ import mongoose from "mongoose";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
 
+// Get Admins
+export const getAdmins = async (_, res) => {
+  try {
+    const admins = await User.find({ role: "admin" }).select("-password");
+
+    res.status(200).json(admins);
+  } catch (error) {
+    res.status(200).json({ message: error.message });
+  }
+};
+
