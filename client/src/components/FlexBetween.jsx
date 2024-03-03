12e05e80fd5ee7c7770854cb2d0c0914385b38c0
Semere <semeabe@gmail.com>
2024-03-03 08:00:00 +0000

    Progress on client\src\components\FlexBetween.jsx (+5 lines)

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,3 +1,8 @@
 const { styled } = require("@mui/material");
 const { Box } = require("@mui/system");
 
+// Flex Between
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
