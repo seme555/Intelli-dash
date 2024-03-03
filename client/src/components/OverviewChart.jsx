31069d459d5a000e8f0f774b1d3aa898d3c0a82b
Semere <semeabe@gmail.com>
2024-03-03 17:00:00 +0000

    Progress on client\src\components\OverviewChart.jsx (+9 lines)

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -3,3 +3,12 @@ import { ResponsiveLine } from "@nivo/line";
 import { useTheme, CircularProgress, Box } from "@mui/material";
 
 import { useGetSalesQuery } from "state/api";
+
+// Overview Chart
+const OverviewChart = ({ isDashboard = false, view }) => {
+  // theme
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
+
+  // get chart data
+  const [totalSalesLine, totalUnitsLine] = useMemo(() => {
