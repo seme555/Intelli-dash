c9dad8deeedca346443615c74d7e7fbfa9553367
Semere <semeabe@gmail.com>
2024-03-03 07:00:00 +0000

    Progress on client\src\scenes\transactions\index.jsx (+8 lines)

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -1,3 +1,11 @@
 import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { Box, useTheme } from "@mui/material";
+
+import { useGetTransactionsQuery } from "state/api";
+import { Header, DataGridCustomToolbar } from "components";
+
+// Transactions
+const Transactions = () => {
+  // theme
+  const theme = useTheme();
