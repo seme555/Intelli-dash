1e34287d3bcb8d5650fa81db4f38c72070b0792f
Semere <semeabe@gmail.com>
2024-03-05 13:00:00 +0000

    Progress on client\src\scenes\index.js (+11 lines)

--- client/src/scenes/index.js
+++ client/src/scenes/index.js
@@ -3,3 +3,14 @@
 
 import Layout from "./layout";
 import Dashboard from "./dashboard";
+import Products from "./products";
+import Customers from "./customers";
+import Transactions from "./transactions";
+import Geography from "./geography";
+import Overview from "./overview";
+import Daily from "./daily";
+import Monthly from "./monthly";
+import Breakdown from "./breakdown";
+import Admin from "./admin";
+import Performance from "./performance";
+
