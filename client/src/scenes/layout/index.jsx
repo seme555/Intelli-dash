34c01652e70983fbfed4a3db322fbddd6d6812d3
Semere <semeabe@gmail.com>
2024-03-07 14:00:00 +0000

    Progress on client\src\scenes\layout\index.jsx (+12 lines)

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -8,3 +8,15 @@ import { useGetUserQuery } from "state/api";
 
 // Layout
 const Layout = () => {
+  // is desktop
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  // is sidebar open
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  // get user id from redux selector
+  const userId = useSelector((state) => state.global.userId);
+  // get data
+  const { data } = useGetUserQuery(userId);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      {/* Sidebar */}
