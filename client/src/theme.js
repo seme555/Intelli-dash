5d7718e54da55679aa71be0e87e06011fbdcfc46
Semere <semeabe@gmail.com>
2024-03-07 10:00:00 +0000

    Progress on client\src\theme.js (+12 lines)

--- client/src/theme.js
+++ client/src/theme.js
@@ -6,3 +6,15 @@ export const tokensDark = {
     50: "#f0f0f0", // manually adjusted
     100: "#e0e0e0",
     200: "#c2c2c2",
+    300: "#a3a3a3",
+    400: "#858585",
+    500: "#666666",
+    600: "#525252",
+    700: "#3d3d3d",
+    800: "#292929",
+    900: "#141414",
+    1000: "#000000", // manually adjusted
+  },
+  primary: {
+    // blue
+    100: "#d3d4de",
