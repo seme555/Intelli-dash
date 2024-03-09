4a960559ca617576f3ea19a6f906c69e26ec4e74
Semere <semeabe@gmail.com>
2024-03-09 20:00:00 +0000

    Progress on server\models\Transaction.js (+6 lines)

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -4,3 +4,9 @@ import mongoose from "mongoose";
 const TransactionSchema = new mongoose.Schema(
   {
     userId: String,
+    cost: String,
+    products: {
+      type: [mongoose.Types.ObjectId],
+      of: Number,
+    },
+  },
