81278b8e38be8177999e903e2cffd6f97cd5fdda
Semere <semeabe@gmail.com>
2024-03-11 07:00:00 +0000

    Progress on client\src\scenes\customers\index.jsx (+6 lines)

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -3,3 +3,9 @@ import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
 import { useGetCustomersQuery } from "state/api";
+import { Header } from "components";
+
+// Customers
+const Customers = () => {
+  // theme
+  const theme = useTheme();
