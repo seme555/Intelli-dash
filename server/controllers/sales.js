4e4913bfa7600436eeb3ab3fec79daa3477df6b0
Semere <semeabe@gmail.com>
2024-03-13 07:00:00 +0000

    Progress on server\controllers\sales.js (+6 lines)

--- server/controllers/sales.js
+++ server/controllers/sales.js
@@ -6,3 +6,9 @@ export const getSales = async (_, res) => {
   try {
     const overallStats = await OverallStat.find();
 
+    res.status(200).json(overallStats[0]);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
