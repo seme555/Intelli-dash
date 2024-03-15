9ff110c614c806ccb792145cdfaea54e8d5d5409
Semere <semeabe@gmail.com>
2024-03-15 12:00:00 +0000

    Progress on client\src\state\index.js (+7 lines)

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -10,3 +10,10 @@ const initialState = {
 export const globalSlice = createSlice({
   name: "global",
   initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+  },
+});
+
