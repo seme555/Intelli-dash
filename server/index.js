d86d01b37363988bc28b993bc9a6b56e6fe95ce0
Semere <semeabe@gmail.com>
2024-03-19 07:00:00 +0000

    Progress on server\index.js (+8 lines)

--- server/index.js
+++ server/index.js
@@ -13,3 +13,11 @@ import { rateLimiter } from "./middlewares/rateLimiter.js";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// Data imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
