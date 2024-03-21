efdf328b9b1e59e0cbd7017db612f9173892f334
Semere <semeabe@gmail.com>
2024-03-21 22:00:00 +0000

    Progress on client\src\scenes\geography\index.jsx (+9 lines)

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -17,3 +17,12 @@ const Geography = () => {
     <Box m="1.5rem 2.5rem">
       {/* Header */}
       <Header title="GEOGRAPHY" subtitle="Find where your users are located." />
+
+      {/* Content */}
+      <Box
+        mt="40px"
+        height="75vh"
+        border={`1px solid ${theme.palette.secondary[200]}`}
+        borderRadius="4px"
+      >
+        {/* choropleth map */}
