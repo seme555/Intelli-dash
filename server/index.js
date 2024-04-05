53350001c8141f644c6c66cfbdba582b448935e9
Semere <semeabe@gmail.com>
2024-04-05 07:00:00 +0000

    Progress on server\index.js (+5 lines)

--- server/index.js
+++ server/index.js
@@ -21,3 +21,8 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
