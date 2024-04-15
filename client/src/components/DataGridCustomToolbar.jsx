0943aea557c453962aac13e7cfd0520ee86c625e
Semere <semeabe@gmail.com>
2024-04-15 00:00:00 +0000

    Progress on client\src\components\DataGridCustomToolbar.jsx (+11 lines)

--- client/src/components/DataGridCustomToolbar.jsx
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -22,3 +22,14 @@ const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
           <GridToolbarExport />
         </FlexBetween>
 
+        {/* Right Side (search) */}
+        <TextField
+          label="Search..."
+          sx={{ mb: "0.5rem", width: "15rem" }}
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          variant="standard"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
