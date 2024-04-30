3a60c1faa002b847b7e8680384113091371c0ce3
Semere <semeabe@gmail.com>
2024-04-30 08:00:00 +0000

    Progress on server\controllers\client.js (+12 lines)

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -28,3 +28,15 @@ export const getProducts = async (_, res) => {
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
+};
+
+// Get Customers
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
