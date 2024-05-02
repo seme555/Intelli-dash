b2b129eee7793469758471d8103c853997e8a11a
Semere <semeabe@gmail.com>
2024-05-02 14:00:00 +0000

    Progress on client\src\scenes\customers\index.jsx (+8 lines)

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -31,3 +31,11 @@ const Customers = () => {
     },
     {
       field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1) $2-$3"); // format phone number (123) 456-7890
+      },
+    },
+    {
+      field: "country",
